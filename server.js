7f84f3837fcb11d24a353153ca4cea002c64db7e
Morgan Bickle <morgan.bickle@gmail.com>
2015-01-29 15:42:04 -0600

    changed main script and added dependencies

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 var express        =        require("express");
 var bodyParser     =        require("body-parser");
+var util           =        require("util");
 var app            =        express();
 app.use(bodyParser.json());
-app.post('/login',function(req,res){
-  var user_name=req.body.user;
-  var password=req.body.password;
-  console.log("User name = "+user_name+", password is "+password);
-  res.end("yes");
+app.get('/echo',function(req,res){
+  console.log("GET: "+util.inspect(req));
+  res.end({'_id':new Date().toString()});
+});
+app.put('/echo',function(req,res){
+  console.log("PUT: "+util.inspect(req));
+  console.log("PUT: "+req.body);
+  res.end(res.body);
+});
+app.post('/echo',function(req,res){
+  console.log("POST: "+util.inspect(req));
+  console.log("POST: "+res.body);
+  res.end(res.body);
+});
+app.delete('/echo',function(req,res){
+  console.log("DELETE: "+util.inspect(req));
+  console.log("DELETE: "+res.body);
+  res.end(res.body);
 });
 
 app.listen(3000,function(){
